8c7d7d2d3333da2d448b02400259864261c798e0
Josh-Twomey <140522624+Josh-Twomey@users.noreply.github.com>
2024-08-29 13:00:33 +0200

    Supabase auth update

--- src/components/Auth/Auth.Presentation.tsx
+++ src/components/Auth/Auth.Presentation.tsx
@@ -28,7 +28,16 @@ const Base = styled(Paper)`
 
 
 export const AuthPresentation = () => {
+  const navigate = useNavigate();
+
   
+  useEffect(() => {
+    supabase.auth.onAuthStateChange(async (event) => {
+      if (event === "SIGNED_IN") {
+        navigate("/list");
+      } 
+    });
+  },[])
 
   return (
     <Wrapper>
